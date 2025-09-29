3360bb29950b46457b3f0295f914b3605a63da23
Karharu <macabalejose@gmail.com>
2025-09-30 01:37:12 +0800

    Update Game

--- Game.java
+++ Game.java
@@ -17,10 +17,11 @@ public class Game{
 
     JFrame window;
     Container con;
-    JPanel titleNamePanel, startButtonPanel, mainTextPanel, choiceButtonPanel;
-    JLabel titleNameLabel;
+    JPanel titleNamePanel, startButtonPanel, mainTextPanel, choiceButtonPanel, playerPanel;
+    JLabel titleNameLabel, hpLabel, hpLabelNumber, weaponLabel, weaponLabelName;
     Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
     Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
+    Font choiceFont = new Font("Times New Roman", Font.PLAIN, 17);
     JButton startButton, choice1, choice2, choice3, choice4;
     JTextArea mainTextArea;
 
@@ -100,26 +101,68 @@ public class Game{
         choice1.setBackground(Color.BLACK);
         choice1.setForeground(Color.WHITE);
         choice1.setFocusPainted(false);
-        choice1.setFont(normalFont);
+        choice1.setFont(choiceFont);
         choiceButtonPanel.add(choice1);
         choice2 = new JButton("Choice 2");
         choice2.setBackground(Color.BLACK);
         choice2.setForeground(Color.WHITE);
         choice2.setFocusPainted(false);
-        choice2.setFont(normalFont);
+        choice2.setFont(choiceFont);
         choiceButtonPanel.add(choice2);
         choice3 = new JButton("Choice 3");
         choice3.setBackground(Color.BLACK);
         choice3.setForeground(Color.WHITE);
         choice3.setFocusPainted(false);
-        choice3.setFont(normalFont);
+        choice3.setFont(choiceFont);
         choiceButtonPanel.add(choice3);
         choice4 = new JButton("Choice 4");
         choice4.setBackground(Color.BLACK);
         choice4.setForeground(Color.WHITE);
         choice4.setFocusPainted(false);
-        choice4.setFont(normalFont);
+        choice4.setFont(choiceFont);
         choiceButtonPanel.add(choice4);
+
+        playerPanel = new JPanel();
+        playerPanel.setBounds(100, 15, 600, 50);
+        playerPanel.setBackground(Color.BLACK);
+        playerPanel.setLayout(new GridLayout(1, 4));
+        con.add(playerPanel);
+        hpLabel = new JLabel("HP:");
+        hpLabel.setFont(normalFont);
+        hpLabel.setForeground(Color.WHITE);
+        playerPanel.add(hpLabel);
+        hpLabelNumber = new JLabel();
+        hpLabelNumber.setFont(normalFont);
+        hpLabelNumber.setForeground(Color.WHITE);
+        playerPanel.add(hpLabelNumber);
+        weaponLabel = new JLabel("Weapon:");
+        weaponLabel.setFont(normalFont);
+        weaponLabel.setForeground(Color.WHITE);
+        playerPanel.add(weaponLabel);
+        weaponLabelName = new JLabel();
+        weaponLabelName.setFont(normalFont);
+        weaponLabelName.setForeground(Color.WHITE);
+        playerPanel.add(weaponLabelName);
+
+        playerSetUp();
+    }
+
+    public void playerSetUp(){
+        playerHP = 15;
+        playerWeapon = "Dagger";
+        weaponLabelName.setText(playerWeapon);
+        hpLabelNumber.setText("" + playerHP);
+
+        townGate();
+    }
+
+    public void townGate(){
+        mainTextArea.setText("The Rogue receives a mysterious letter sealed\nwith no crest. It offers a fortune if they succeed\nin a single task: “Slay the King who sits upon\nthe golden throne.”");
+        
+        choice1.setText("Accept the bounty with excitement");
+        choice2.setText("Accept reluctantly");
+        choice3.setText("Refuse at first, but keep the letter");
+        choice4.setText("Burn the letter");
     }
 
     public class TitleScreenHandler implements ActionListener{
@@ -133,22 +176,4 @@ public class Game{
     public static void main(String[] args){
         new Game();
     }
-
-//     public static void main(String[] args){
-//         Game ChaKiTim = new Game();
-//         ChaKiTim.playerSetUp();
-//         ChaKiTim.townGate();
-//    }
-
-//    public void playerSetUp(){
-//         playerHP = 10;
-//         monsterHP = 15;
-
-//         playerWeapon = "Knife";
-
-//         System.out.println("Your HP: " + playerHP);
-//         System.out.println("Your Weapon: " + playerWeapon);
-        
-//         System.out.println("Please enter your name: ");
-//    }
 }
